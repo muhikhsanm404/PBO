73f92a8c30b14d8efd27158dd144d8f2ec7ee00b
muhikhsanm404 <muhikhsanm2018@gmail.com>
2023-01-01 13:25:10 +0700

    Create PPN.java

---
+++ src/PPN.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author Muh Ikhsan M
+ */
+public interface PPN {
+    
+    final double pajak1persen = 0.01;
+    final double pajak2persen = 0.02;
+    final double pajak5persen =0.05;
+ 
+    void hitungHarga();
+}
